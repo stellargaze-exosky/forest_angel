c5b588c1acabd76c1474961cc0a39cce961d2c99
Devesh Kumar <39146448+mathematicie@users.noreply.github.com>
2023-09-09 23:13:06 +0530

    Add files via upload

---
+++ About.java
@@ -0,0 +1,17 @@
+package com.example.foresttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        if (getSupportActionBar()!=null)
+            getSupportActionBar().hide();
+    }
+}
